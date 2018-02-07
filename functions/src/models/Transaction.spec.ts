89489a25f984cf5b40676ba816735e921f0fdb96
Joe Wemyss <support@javathenutt.com>
2018-02-07 17:22:43 +0000

    created transaction model
    Created the model that will be used to track Transactions (income/expenditure)

---
+++ functions/src/models/Transaction.spec.ts
@@ -0,0 +1,31 @@
+import Transaction from './Transaction';
+import {expect} from 'chai';
+import * as moment from 'moment';
+import 'mocha';
+
+describe('Transaction.ts', () => {
+	it('should create an object with default constructor', () => {
+		const transaction = new Transaction();
+		expect(transaction).to.be.instanceOf(Transaction);
+	});
+	describe('clone', () => {
+		it('should return an object which deeply, but not strictly, equals the original', () => {
+			const transaction = new Transaction('someid', 'wages', 2333, 'Monthly', '2018-09-09');
+			const newTransaction = transaction.clone();
+			expect(transaction).to.not.equal(newTransaction);
+			expect(transaction).to.eql(newTransaction);
+		});
+	});
+	describe('isFuture', ()=>{
+		it('should return true when the due date is in the future', () => {
+			const date = moment().add(10, 'days').format('YYYY-MM-DD');
+			const transaction = new Transaction('someid', 'wages', 2333, 'Monthly', date);
+			expect(transaction.isFuture()).to.be.true;
+		});
+		it('should return false when the due date is not in the future', () => {
+			const date = moment().subtract(10, 'days').format('YYYY-MM-DD');
+			const transaction = new Transaction('someid', 'wages', 2333, 'Monthly', date);
+			expect(transaction.isFuture()).to.be.false;
+		});
+	});
+});
---
+++ functions/src/models/Transaction.ts
@@ -0,0 +1,67 @@
+import * as moment from 'moment';
+export default class Transaction {
+	private _id: string;
+	private _title: string;
+	private _amount: number;
+	private _frequency: string;
+	private _nextDueDate: string;
+
+
+	constructor(id?: string, title?: string, amount?: number, frequency?: string, nextDueDate?: string) {
+		this._id = id;
+		this._title = title;
+		this._amount = amount;
+		this._frequency = frequency;
+		this._nextDueDate = nextDueDate;
+	}
+
+
+	get id(): string {
+		return this._id;
+	}
+
+	set id(value: string) {
+		this._id = value;
+	}
+
+	get title(): string {
+		return this._title;
+	}
+
+	set title(value: string) {
+		this._title = value;
+	}
+
+	get amount(): number {
+		return this._amount;
+	}
+
+	set amount(value: number) {
+		this._amount = value;
+	}
+
+	get frequency(): string {
+		return this._frequency;
+	}
+
+	set frequency(value: string) {
+		this._frequency = value;
+	}
+
+	get nextDueDate(): string {
+		return this._nextDueDate;
+	}
+
+	set nextDueDate(value: string) {
+		this._nextDueDate = value;
+	}
+
+	public isFuture(): boolean {
+		const date = moment(this.nextDueDate);
+		return date.isSameOrAfter(moment());
+	}
+
+	public clone(){
+		return new Transaction(this.id, this.title, this.amount, this.frequency, this.nextDueDate)
+	}
+}

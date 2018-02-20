e3b3522a8ab20f949a34a7d06557d68d6cc4df47
Joe Wemyss <support@javathenutt.com>
2018-02-20 01:35:13 +0000

    Restructured Transaction Service
    Restructured the transactions service so that it can fetch not only incomes, but also expenditures, as well as being able to fetch both together. I also added the property to the model that determines the type of transaction veing operated on. There is also now a set of functiona that can be used to map the data for delivery to the client.

--- functions/src/service/TransactionService.spec.ts
+++ functions/src/service/TransactionService.spec.ts
@@ -1,7 +1,7 @@
 import * as chai from 'chai';
 import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
-import { getFutureTransactions, getNextDate, getFutureIncomes } from './TransactionService';
+import { generateFutureTransactions, getNextDate, getFutureIncomes } from './TransactionService';
 import * as FirebaseUtil from '../util/Firestore';
 import 'mocha';
 import * as moment from 'moment';
@@ -13,66 +13,66 @@ const expect = chai.expect;
 const sandbox = sinon.sandbox.create();
 describe('TransactionService', () => {
 	afterEach(() => sandbox.restore())
-	describe('getFutureTransactions', () => {
+	describe('generateFutureTransactions', () => {
 		it('should return 12 Weekly transactions by default', () => {
 			const transaction = new Transaction('something', 'wages', 2300, 'Weekly', moment().format('YYYY-MM-DD'));
-			const transactionList = getFutureTransactions(transaction);
+			const transactionList = generateFutureTransactions(transaction);
 			expect(transactionList.length).to.equal(12);
 		});
 		it('should return 3 Monthly transactions by default', () => {
 			const transaction = new Transaction('something', 'wages', 2300, 'Monthly', moment().format('YYYY-MM-DD'));
-			const transactionList = getFutureTransactions(transaction);
+			const transactionList = generateFutureTransactions(transaction);
 			expect(transactionList.length).to.equal(3);
 		});
 		it('should return 90 Daily transactions by default', () => {
 			const transaction = new Transaction('something', 'wages', 2300, 'Daily', moment().format('YYYY-MM-DD'));
-			const transactionList = getFutureTransactions(transaction);
+			const transactionList = generateFutureTransactions(transaction);
 			expect(transactionList.length).to.equal(90);
 		});
 		it('should return 4 Weekly transactions when one month is specified', () => {
 			const transaction = new Transaction('something', 'wages', 2300, 'Weekly', moment().format('YYYY-MM-DD'));
-			const transactionList = getFutureTransactions(transaction, 1);
+			const transactionList = generateFutureTransactions(transaction, 1);
 			expect(transactionList.length).to.equal(4);
 		});
 		it('should return 1 Monthly transactions when one month is specified', () => {
 			const transaction = new Transaction('something', 'wages', 2300, 'Monthly', moment().format('YYYY-MM-DD'));
-			const transactionList = getFutureTransactions(transaction, 1);
+			const transactionList = generateFutureTransactions(transaction, 1);
 			expect(transactionList.length).to.equal(1);
 		});
 		it('should return 30 Daily transactions when one month is specified', () => {
 			const transaction = new Transaction('something', 'wages', 2300, 'Daily', moment().format('YYYY-MM-DD'));
-			const transactionList = getFutureTransactions(transaction, 1);
+			const transactionList = generateFutureTransactions(transaction, 1);
 			expect(transactionList.length).to.equal(30);
 		});
 		it('should return only future transactions', () => {
 			const transaction = new Transaction('something', 'wages', 2300, 'Daily', moment().subtract(10, 'days').format('YYYY-MM-DD'));
-			const transactionList = getFutureTransactions(transaction, 1);
+			const transactionList = generateFutureTransactions(transaction, 1);
 			expect(moment(transactionList[0].nextDueDate).isSameOrAfter(moment().subtract(1, 'day'))).to.be.true;
 		});
 		it('should skip the specified number of months', () => {
 			const transaction = new Transaction('something', 'wages', 2300, 'Daily', moment().format('YYYY-MM-DD'));
-			const transactionList = getFutureTransactions(transaction, 1, 3);
+			const transactionList = generateFutureTransactions(transaction, 1, 3);
 			expect(moment(transactionList[0].nextDueDate)
 				.isSameOrAfter(moment().add(9, 'weeks'))).to.be.true;
 		});
 		it('should return sporadic transactions wrapped in an array when they are in the future', () => {
 			const transaction = new Transaction('something', 'wages', 2300, 'Sporadic', moment().add(1, 'day').format('YYYY-MM-DD'));
-			const transactionList = getFutureTransactions(transaction);
+			const transactionList = generateFutureTransactions(transaction);
 			expect(transactionList).to.eql([transaction]);
 		});
 		it('should return an empty array when sporadic transactions are not in the future', () => {
 			const transaction = new Transaction('something', 'wages', 2300, 'Sporadic', moment().subtract(1, 'day').format('YYYY-MM-DD'));
-			const transactionList = getFutureTransactions(transaction);
+			const transactionList = generateFutureTransactions(transaction);
 			expect(transactionList).to.eql([]);
 		});
 		it('should return once transactions wrapped in an array', () => {
 			const transaction = new Transaction('something', 'wages', 2300, 'Once', moment().add(1, 'day').format('YYYY-MM-DD'));
-			const transactionList = getFutureTransactions(transaction);
+			const transactionList = generateFutureTransactions(transaction);
 			expect(transactionList).to.eql([transaction]);
 		});
 		it('should return an empty array when once transactions are not in the future', () => {
 			const transaction = new Transaction('something', 'wages', 2300, 'Once', moment().subtract(1, 'day').format('YYYY-MM-DD'));
-			const transactionList = getFutureTransactions(transaction);
+			const transactionList = generateFutureTransactions(transaction);
 			expect(transactionList).to.eql([]);
 		});
 	});
--- functions/src/service/TransactionService.ts
+++ functions/src/service/TransactionService.ts
@@ -4,27 +4,46 @@ import Result from "../dto/Result";
 import { fetchCollection } from '../util/Firestore';
 
 export const getFutureIncomes =
-	async (userId: string, months?: number, skip?: number): Promise<Result> => {
+	async (userId: string, months?: number, skip?: number): Promise<Result> => await fetchFutureTransactions(userId, 'Income', months, skip);
+export const getFutureExpenditures = async (userId: string, months?: number, skip?: number): Promise<Result> => await fetchFutureTransactions(userId, 'Expenditure', months, skip);
+export const getFutureTransactions = async (userId: string, months?: number, skip?: number): Promise<Result> => await fetchFutureTransactions(userId, 'all', months, skip);
+export const fetchFutureTransactions = async(userId: string, type: 'Income' | 'Expenditure' | 'all', months?: number, skip?: number) : Promise<Result> => {
 	console.log('getting future transactions for ', userId);
-	const incomes: Result = await fetchCollection(`transactions/${userId}/incomes`);
+	const incomes: Result = await fetchFromDb(userId, type);
 	if(!incomes.success) return incomes;
+	console.log('result of fetching incomes:', incomes);
 	let results : Array<Transaction> = [];
 	incomes.data.forEach(income => {
 		console.log('iterating through fetched results');
-		if(!income.isRecurring() && income.isFuture()) {
+		const newIncome: Transaction = mapTransaction(income, type !== 'all' ? type : income.type);
+		if(!newIncome.isRecurring() && newIncome.isFuture()) {
 			console.log('income is non recurring and is in the future, adding single entity');
-			results = results.concat([income])
-		} else if(income.isRecurring()) {
+			results = results.concat([newIncome])
+		} else if(newIncome.isRecurring()) {
 			console.log('income is recurring, generating multiple entities');
-			results = results.concat(getFutureTransactions(income, months, skip))
+			results = results.concat(generateFutureTransactions(newIncome, months, skip))
 		}
 	});
 	console.log('entity generation complete, generated', results.length, 'entries');
-	incomes.data = results;
+	const mappedResults = results.map(result => result.mapForClient());
+	incomes.data = mappedResults;
 	return incomes;
 };
-
-export const getFutureTransactions =
+const fetchFromDb = async (userId: string, type: 'Income' | 'Expenditure' | 'all'): Promise<Result> => {
+	console.log('fetching', type, 'from db for', userId);
+	const dbPath = type === 'Income' ? 'incomes' : 'expenditures';
+	if(type !== 'all') return await fetchCollection(`transactions/${userId}/${dbPath}`);
+	const incomes = await fetchCollection(`transactions/${userId}/incomes`);
+	if(!incomes.success) return incomes;
+	incomes.data = incomes.data.map(income => Object.assign({type: 'Income'}, income));
+	console.log('new incomes', incomes);
+	const expenditures = await fetchCollection(`transactions/${userId}/expenditures`);
+	if (!expenditures.success) return expenditures;
+	expenditures.data = expenditures.data.map(income => Object.assign({type: 'Expenditure'}, income));
+	incomes.data = incomes.data.concat(expenditures.data);
+	return incomes;
+};
+export const generateFutureTransactions =
 	(transaction: Transaction, months: number = 3, skip: number = 0): Array<Transaction>  =>{
 	console.log('attempting to get', months, 'months worth of transactions skipping', skip);
 	if(!transaction.isRecurring() && transaction.isFuture()) return [transaction];
@@ -73,6 +92,11 @@ const incrementDate = (date: string, frequency: string, amount: number = 1): str
 	};
 	console.log('incrementing', date, 'by', amount, INTERVALS[frequency]);
 	return moment(date).add(amount, INTERVALS[frequency]).format('YYYY-MM-DD');
+};
+export const mapTransactions = (transactionList: Array<{id:string, frequency: string, amount: number, nextDueDate: string, title: string}>, type: 'Income' | 'Expenditure'): Array<Transaction> => transactionList.map(transaction => mapTransaction(transaction, type));
+export const mapTransaction =  (transactionObj: {id:string, frequency: string, amount: number, nextDueDate: string, title: string}, type: 'Income' | 'Expenditure'): Transaction => {
+	console.log('converting', transactionObj, 'to a transaction object of type', type);
+	return new Transaction(transactionObj.id, transactionObj.title, transactionObj.amount, transactionObj.frequency, transactionObj.nextDueDate, type)
 };
 
 

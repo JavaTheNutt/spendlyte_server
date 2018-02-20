83c50051d637d6d251e839d5ab03629f216b4f17
Joe Wemyss <support@javathenutt.com>
2018-02-20 01:41:20 +0000

    added new routes ot finance app

--- functions/src/apps/finance/routes.ts
+++ functions/src/apps/finance/routes.ts
@@ -1,7 +1,23 @@
-import { Application, Request, Response } from "express";
-
+import { Application, Response } from "express";
+import { IRequest } from "../../../types/c_express";
+import { getFutureIncomes, getFutureExpenditures, getFutureTransactions } from '../../service/TransactionService'
 export default (app: Application) => {
-	app.get('/', (req: Request, res: Response) => {
-		res.status(200).send('working');
+	app.get('/income', async (req: IRequest, res: Response) => {
+		console.log('request recieved to fetch all future incomes');
+		const result = await getFutureIncomes(req.user.uid, req.query.num, req.query.skip);
+		console.log('result', result);
+		res.status(result.success ? 200: 500).send(result.data);
+	});
+	app.get('/expenditure', async (req: IRequest, res: Response) => {
+		console.log('request recieved to fetch all future expenditures');
+		const result = await getFutureExpenditures(req.user.uid, req.query.num, req.query.skip);
+		console.log('result', result);
+		res.status(result.success ? 200: 500).send(result.data);
+	});
+	app.get('/all', async (req: IRequest, res: Response) => {
+		console.log('request recieved to fetch all future expenditures');
+		const result = await getFutureTransactions(req.user.uid, req.query.num, req.query.skip);
+		console.log('result', result);
+		res.status(result.success ? 200: 500).send(result.data);
 	})
 }

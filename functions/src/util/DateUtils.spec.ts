2f24ab7f642cd59756759bd6152565640087e5c6
Joe Wemyss <support@javathenutt.com>
2018-02-21 18:51:41 +0000

    Added new date utils
    Added date utils to fetch the start and end date bounds based off the number of months to be fetched and the number to be skipped, as well as a function to determine if a specified date falls between two other dates.

--- functions/src/util/DateUtils.spec.ts
+++ functions/src/util/DateUtils.spec.ts
@@ -3,7 +3,7 @@ import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
 import 'mocha';
 
-import { getSkipFromMonthNum } from "./DateUtils";
+import { getSkipFromMonthNum, getFetchPeriod, fallsInFetchPeriod } from "./DateUtils";
 import moment = require("moment");
 
 chai.use(sinonChai);
@@ -21,6 +21,35 @@ describe('DateUtils.ts', () => {
 		});
 		it('should return 0 when the current month is passed', () => {
 			expect(getSkipFromMonthNum(+moment().format('M'))).to.eql(0);
-		})
+		});
+	});
+	describe('getFetchPeriod', () => {
+		it('should return today as the start, and one month from today as the end by default', () => {
+			const result = getFetchPeriod();
+			expect(result.start, 'start does not match').to.equal(moment().format('YYYY-MM-DD'));
+			expect(result.end, 'end does not match').to.equal(moment().add(1, 'months').format('YYYY-MM-DD'));
+		});
+		it('should return today as the start and two months from today when num is set to two', () => {
+			const result = getFetchPeriod(2);
+			expect(result.start, 'start does not match').to.equal(moment().format('YYYY-MM-DD'));
+			expect(result.end, 'end does not match').to.equal(moment().add(2, 'months').format('YYYY-MM-DD'));
+		});
+		it('should return one month from today as the start and three months from today as the end when num is set to two and skip is set to one', () => {
+			const result = getFetchPeriod(2, 1);
+			expect(result.start, 'start does not match').to.equal(moment().add(1, 'months').format('YYYY-MM-DD'));
+			expect(result.end, 'end does not match').to.equal(moment().add(3, 'months').format('YYYY-MM-DD'));
+		});
+	});
+	describe('fallsInFetchPeriod', () => {
+		it('should return true for dates that fall in the fetch period', () => {
+			let startDate = moment().subtract(1, 'day').format('YYYY-MM-DD');
+			let endDate = moment().add(1, 'day').format('YYYY-MM-DD');
+			expect(fallsInFetchPeriod({start: startDate, end: endDate}, moment().format('YYYY-MM-DD'))).to.be.true;
+		});
+		it('should return false for dates that fall outside of the fetch period', () => {
+			let endDate = moment().subtract(1, 'day').format('YYYY-MM-DD');
+			let startDate = moment().add(1, 'day').format('YYYY-MM-DD');
+			expect(fallsInFetchPeriod({start: startDate, end: endDate}, moment().format('YYYY-MM-DD'))).to.be.false;
+		});
 	})
 });
--- functions/src/util/DateUtils.ts
+++ functions/src/util/DateUtils.ts
@@ -5,3 +5,18 @@ export const getSkipFromMonthNum = (month: number): number => {
 	console.log('current month number', currentMonthNum);
 	return month >= currentMonthNum ? month - currentMonthNum : 12 - ((month - currentMonthNum) * -1);
 };
+
+export const getFetchPeriod = (amount: number = 1, skip: number = 0): {start: string, end: string} => {
+	console.log('getting fetch period of', amount, 'months skipping', skip);
+	const startDate = moment().add(skip, 'months');
+	return {
+		start: startDate.format('YYYY-MM-DD'),
+		end: startDate.add(amount, 'months').format('YYYY-MM-DD')
+	}
+};
+
+export const fallsInFetchPeriod = (period: {start: string, end: string}, date: string):boolean => {
+	console.log('testing if', date, 'falls between', period.start, 'and', period.end);
+	const dateMoment = moment(date);
+	return dateMoment.isSameOrBefore(period.end) && dateMoment.isSameOrAfter(period.start);
+};
